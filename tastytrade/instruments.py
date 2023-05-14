d317d13420063ef61edd6c4a213a1895b79f1807
petycomppety <30414255+petycomppety@users.noreply.github.com>
2023-05-14 20:25:21 +0200

    Crypto dataclass and minor fixes

--- tastytrade/instruments.py
+++ tastytrade/instruments.py
@@ -1,15 +1,41 @@
 import json
 from typing import List
+from dataclasses import dataclass
 
 import requests
 
 from tastytrade.session import Session
+from utils import validate_response
+
+
+@dataclass
+class DestinationVenueSymbol:
+    id: int
+    symbol: str
+    destination_venue: str
+    routable: bool
+    max_quantity_precision: int = None
+    max_price_precision: int = None
+
+
+@dataclass
+class Cryptocurrency:
+    id: int
+    symbol: str
+    instrument_type: str
+    short_description: str
+    description: str
+    is_closing_only: bool
+    active: bool
+    tick_size: str
+    streamer_symbol: str
+    destination_venue_symbols: List[DestinationVenueSymbol]
 
 
 class Instruments:
     def get_cryptocurrencies(
         self, session: Session, symbols: List[str] = None
-    ) -> List[dict]:
+    ) -> List[Cryptocurrency]:
         if symbols:
             symbol_params = "&".join([f"symbol[]={s}" for s in symbols])
             url = f"{session.base_url}/instruments/cryptocurrencies?{symbol_params}"
@@ -17,11 +43,23 @@ class Instruments:
             url = f"{session.base_url}/instruments/cryptocurrencies"
 
         response = requests.get(url, headers=session.headers)
-        if response.status_code == 200:
-            response_data = json.loads(response.content)
-            cryptocurrencies = response_data["data"]["items"]
-            return cryptocurrencies
-        else:
-            raise Exception(
-                f"Error getting cryptocurrencies: {response.status_code} - {response.content.decode('utf-8')}"
+        validate_response(response)
+        response_data = json.loads(response.content)
+        cryptocurrencies_data = response_data["data"]["items"]
+        cryptocurrencies = []
+        for data in cryptocurrencies_data:
+            data = {
+                k.replace("-", "_"): v for k, v in data.items()
+            }  # replace hyphens with underscores in keys
+            destination_venue_symbols = [
+                DestinationVenueSymbol(
+                    **{k.replace("-", "_"): v for k, v in dvs.items()}
+                )
+                for dvs in data.pop("destination_venue_symbols")
+            ]
+            cryptocurrencies.append(
+                Cryptocurrency(
+                    destination_venue_symbols=destination_venue_symbols, **data
+                )
             )
+        return cryptocurrencies
