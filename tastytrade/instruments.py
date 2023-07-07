95d5008c0b40d46ab781090ca50f2c9d53e027d1
maneum1 <martin.neumann@nsyss.com>
2023-07-07 19:01:49 -0400

    get_futures() method works again after removing some class variables (#65)
    * Removed Future variables (streamer_symbol, is_tradeable, future_product) since they are not anymore provided by tasty trade
    
    * Made Future variables (streamer_symbol, is_tradeable, future_product) optional
    
    * Correction on optional Future variables (streamer_symbol, is_tradeable, future_product)

--- tastytrade/instruments.py
+++ tastytrade/instruments.py
@@ -616,11 +616,11 @@ class Future(TradeableTastytradeJsonDataclass):
     product_group: str
     exchange: str
     streamer_exchange_code: str
-    streamer_symbol: str
     back_month_first_calendar_symbol: bool
-    is_tradeable: bool
-    future_product: 'FutureProduct'
     instrument_type: InstrumentType = InstrumentType.FUTURE
+    streamer_symbol: Optional[str] = None
+    is_tradeable: Optional[bool] = None
+    future_product: Optional['FutureProduct'] = None
     contract_size: Optional[Decimal] = None
     main_fraction: Optional[Decimal] = None
     sub_fraction: Optional[Decimal] = None
