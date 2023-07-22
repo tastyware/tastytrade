3a30f2f619bb68dd2decd0661731d51a47de692d
Graeme22 <graeme.holliday@pm.me>
2023-07-21 21:48:04 -0500

    fix build bug with __init__.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import find_packages, setup
 
-from tastytrade import VERSION
 
 f = open('README.rst', 'r')
 LONG_DESCRIPTION = f.read()
@@ -8,7 +7,7 @@ f.close()
 
 setup(
     name='tastytrade',
-    version=VERSION,
+    version='6.0',
     description='An unofficial SDK for Tastytrade!',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
