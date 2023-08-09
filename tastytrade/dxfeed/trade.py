c9cc0d19820cffd0ecbe0c95e56e627a515415f9
Coen Kuijpers <coen.kuijpers@asild.eu>
2023-08-09 12:20:01 +0200

    - rolled back files that didn't changed, but were part of PR anyway

--- tastytrade/dxfeed/trade.py
+++ tastytrade/dxfeed/trade.py
@@ -12,7 +12,6 @@ class Trade(Event):
     about all transactions, but only about the last transaction for a single
     instrument.
     """
-
     #: symbol of this event
     eventSymbol: str
     #: time of this event
