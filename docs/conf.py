6627b5037033a43fbe9a204056a9ab35bec16dd6
Graeme22 <graeme.holliday@pm.me>
2023-12-06 15:35:54 -0500

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'tastytrade'
 copyright = '2023, Graeme Holliday'
 author = 'Graeme Holliday'
-release = '6.3'
+release = '6.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
