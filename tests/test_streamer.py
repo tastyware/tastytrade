d711f9c0f2e758451780a564ff1d7fa167f78ae7
Quenos <coen.kuijpers@asild.eu>
2024-05-26 13:30:36 +0200

    Added tests for DXLinkStreamer.get_event_nowait

--- tests/test_streamer.py
+++ tests/test_streamer.py
@@ -33,8 +33,8 @@ async def test_dxlink_streamer(session):
 
         subs = ['QQQ', 'BA']
         await streamer.subscribe(EventType.QUOTE, subs)
-        await streamer.subscribe(EventType.GREEKS, subs)
-        assert streamer.get_event_nowait(EventType.GREEKS) is None
+        await streamer.subscribe(EventType.TRADE, subs)
+        assert streamer.get_event_nowait(EventType.TRADE) is None
         assert streamer.get_event_nowait(EventType.QUOTE) is not None
         await streamer.unsubscribe_candle(subs[0], '1d')
         await streamer.unsubscribe(EventType.QUOTE, subs[1])
