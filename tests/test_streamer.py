a424fbbc7a35558f1dddc2f0f1130a61fea25730
Quenos <coen.kuijpers@asild.eu>
2024-05-26 14:01:29 +0200

    Added tests for DXLinkStreamer.get_event_nowait

--- tests/test_streamer.py
+++ tests/test_streamer.py
@@ -40,8 +40,5 @@ async def test_dxlink_streamer_nowait(session):
         start_date = datetime.today() - timedelta(days=30)
         await streamer.subscribe_candle(subs, '1d', start_date)
         assert streamer.get_event_nowait(EventType.CANDLE) is not None
-        assert streamer.get_event_nowait(EventType.TRADE) is not None
-        assert streamer.get_event_nowait(EventType.TRADE) is not None
-        assert streamer.get_event_nowait(EventType.TRADE) is None
         await streamer.unsubscribe_candle(subs[0], '1d')
         await streamer.unsubscribe(EventType.QUOTE, subs[1])
