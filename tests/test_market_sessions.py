e66d5d3d461e5fe3948700dc3b30a7153259908c
Joern Blohm <joeblohm@gmail.com>
2025-02-09 09:12:30 +0100

    Added tests for market_sessions module

---
+++ tests/test_market_sessions.py
@@ -0,0 +1,37 @@
+from datetime import date
+
+from tastytrade import Session
+from tastytrade.market_sessions import (
+    a_get_market_time_sessions,
+    a_get_market_time_equity_holidays,
+    a_get_market_state,
+    get_market_time_sessions,
+    get_market_time_equity_holidays,
+    get_market_state,
+)
+
+
+async def test_get_market_time_sessions_async(session: Session):
+    await a_get_market_time_sessions(session, instrument_collections=['Equity','CME','CFE','Smalls'])
+
+
+async def test_get_market_time_equity_holidays_async(session: Session):
+    await a_get_market_time_equity_holidays(session)
+
+
+async def test_get_market_state_async(session: Session):
+    await a_get_market_state(session, instrument_collections=['Equity','CME','CFE','Smalls'])
+
+
+def test_get_market_time_sessions(session: Session):
+    get_market_time_sessions(session, instrument_collections=['Equity','CME','CFE','Smalls'])
+
+
+def test_get_market_time_equity_holidays(session: Session):
+    get_market_time_equity_holidays(session)
+
+
+def test_get_market_state(session: Session):
+    get_market_state(session, instrument_collections=['Equity','CME','CFE','Smalls'])
+
+
