bf7a22675d202fd7aa5c531cbd8aae26d4a0cecb
Emanuel Ramneantu <ramneantu.emanuel@gmail.com>
2026-02-27 19:43:25 +0100

    session client parameters

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import pytest
 from proxy import TestCase
 
@@ -6,14 +9,33 @@ from tastytrade import Session
 pytestmark = pytest.mark.anyio
 
 
+@pytest.fixture(scope="module")
+def session_with_kwargs(anyio_backend: str) -> Session:
+    return Session(os.environ["TT_SECRET"], os.environ["TT_REFRESH"], timeout=10.0)
+
+
 async def test_get_customer(session: Session):
     await session.get_customer()
 
 
-def test_serialize_deserialize(session: Session):
+def test_serialize_deserialize(session: Session, session_with_kwargs: Session):
+    # No client_kwargs
     data = session.serialize()
     obj = Session.deserialize(data)
     assert set(obj.__dict__.keys()) == set(session.__dict__.keys())
+    assert obj.client_kwargs == session.client_kwargs
+    # Simulate legacy serialized payloads that didn't include client_kwargs.
+    legacy_payload = json.loads(data)
+    del legacy_payload["client_kwargs"]
+    legacy_obj = Session.deserialize(json.dumps(legacy_payload))
+    assert legacy_obj.client_kwargs == {}
+    # With timeout client_kwargs
+    data_with_kwargs = session_with_kwargs.serialize()
+    obj_with_kwargs = Session.deserialize(data_with_kwargs)
+    assert set(obj_with_kwargs.__dict__.keys()) == set(
+        session_with_kwargs.__dict__.keys()
+    )
+    assert obj_with_kwargs.client_kwargs == session_with_kwargs.client_kwargs
 
 
 @pytest.mark.usefixtures("inject_credentials")
